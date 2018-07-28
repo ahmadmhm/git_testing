8a93120c9240f12fa604a01116c335c06a9ed2d1
ahmadammhm <ahmadmohammadi940@gmail.com>
2018-07-28 23:47:34 +0430

    1 branch

--- g.php
+++ g.php
@@ -4,3 +4,11 @@ git remote add origin https://github.com/ahmadmhm/git_testing
 
 -------------------
 git add .
+git commit -m "g is changed"
+git push
+-------------------
+git log
+
+git branch ahmad1.0
+git branch
+
